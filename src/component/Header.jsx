9e92b948a1d280a5abaf8542406dadd2f804daab
ubaid1321 <ubaidmir623@gmail.com>
2023-12-14 11:56:15 +0530

    12

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => {
 
     return (
         <div className='shadow-md w-full fixed top-0 left-0'>
-           <div className='md:flex items-center justify-between bg-white py-4 md:px-11 px-7'>
+           <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
             {/* logo section */}
             <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
                 <BookOpenIcon className='w-7 h-7 text-blue-600'/>
-                <span>Inscribe</span>
+                <span>Logo</span>
             </div>
             {/* Menu icon */}
             <div onClick={()=>setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
