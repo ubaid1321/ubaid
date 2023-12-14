2a380c0ae2ab8569e7e92a82a3aea88a270d3d46
ubaid1321 <ubaidmir623@gmail.com>
2023-12-14 11:54:43 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+import Header from "./component/Header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <div className="bg-indigo-600 w-full h-screen">
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
